371958fabceff14c824af93586078fc6463b38a1
Mat <mathieu.k.legault@gmail.com>
2019-08-09 16:20:40 -0400

    style(css): spacing items

--- src/joblist/ExtendedDisplay.js
+++ src/joblist/ExtendedDisplay.js
@@ -4,16 +4,21 @@ import { Row, Col, Button } from 'react-bootstrap';
 
 export default function ExtendedDisplay(props) {
   return (
-    <tr>
+    <tr className="extended-display">
       <td colSpan="5">
-        <Row onClick={props.onClick}>
-          <Col><span>Company:</span> {props.value.company}</Col>
-          <Col><span>Recruiters:</span> {props.value.recruiters}</Col>
-        </Row>
-        <Row onClick={props.onClick}>
-          <Col><span>Title:</span> {props.value.title}</Col>
-          <Col><span>Date:</span> {props.value.date}</Col>
-        </Row>
+        <div onClick={props.onClick}>
+          <Row>
+            <Col className="label-value"><span className="label-type">Company :</span>{props.value.company}</Col>
+            <Col className="label-value"><span className="label-type">Recruiters :</span> {props.value.recruiters}</Col>
+          </Row>
+          <Row>
+            <Col className="label-value"><span className="label-type">Title :</span> {props.value.title}</Col>
+            <Col className="label-value"><span className="label-type">Date :</span> {props.value.date}</Col>
+          </Row>
+          <Row>
+            <Col className="label-value">{props.value.location}</Col>
+          </Row>
+        </div>
         <Row>
           <Col className="spread">
             <Button variant="outline-info">Edit</Button>
