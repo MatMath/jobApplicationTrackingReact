2c94c26366a338e229e176a77783fd8fb750e45e
Mat <mathieu.k.legault@gmail.com>
2019-10-02 12:49:25 -0400

    replicate structure for Recruiters table

--- src/recruiters/index.js
+++ src/recruiters/index.js
@@ -1,18 +1,96 @@
-import React from 'react';
+// Third party libs
+import React, { useEffect } from "react";
+import { connect } from 'react-redux'
+import { Table, Button } from 'react-bootstrap';
 
-import RecruitersTable from './RecruitersTable';
+// Local components
+import RecruitersRowOptions from './RecruitersRowOptions';
+import { Spinner, DisplayError } from '../utils';
 
-export default class CieContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+// Redux
+import {
+  getList,
+  updateExisting,
+  submitNew,
+  removeIdFromList,
+  setShowNew,
+  setActiveId,
+} from './actions';
 
-  render() {
+function CieTable({
+  error,
+  fetching,
+  list,
+  newItem,
+  showNew,
+  activeId,
+
+  // Functions
+  getList,
+  updateExisting,
+  submitNew,
+  removeIdFromList,
+  setShowNew,
+  setActiveId,
+}) {
+
+  useEffect(() => { getList() }, []);
+  
+  const renderDataRow = (item) => {
+    if (item._id === activeId) {
+      return (
+        <tr key={item._id}><td colSpan='2'>
+          <RecruitersRowOptions removeIdFromList={() => removeIdFromList(item._id)} item={item} clickSaveBtn={updateExisting}></RecruitersRowOptions>
+        </td></tr>
+      );
+    };
     return (
-    <div className='container main-data'>
-      <RecruitersTable></RecruitersTable>
-    </div>
+      <tr key={item._id} onClick={() => setActiveId(item._id)}>
+        <td>{item.name}</td>
+        <td>{item.location}</td>
+      </tr>
     );
   }
+  
+  if (fetching) return ( <Spinner></Spinner>);
+  if (error) return (<DisplayError error={error}></DisplayError>);
+  
+  return (
+    <div className='container main-data'>
+      <h1>LEN: {list.length}</h1> <Button onClick={() => setShowNew(!showNew)}>{(showNew)? 'Hide': 'Add new'}</Button>
+      {(showNew)? <RecruitersRowOptions removeIdFromList={() => setShowNew(false)} item={newItem}  clickSaveBtn={submitNew}></RecruitersRowOptions>: ''}
+      <Table striped bordered hover>
+        <thead onClick={() => setActiveId('')}>
+          <tr>
+            <td>Company</td>
+            <td>Agent</td>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map(item => renderDataRow(item))}
+        </tbody>
+      </Table>
+    </div>
+  )
 }
+
+const mapStateToProps = (props) => ({
+  list: props.recruitList.list,
+  newItem: props.recruitList.newItem,
+  showNew: props.recruitList.showNew,
+  activeId: props.recruitList.activeId,
+  fetching: props.utils.fetching,
+  error: props.utils.error,
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    getList,
+    updateExisting,
+    submitNew,
+    removeIdFromList,
+    setShowNew,
+    setActiveId,
+  }
+)(CieTable)
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import newJob from './newJob_data';
 import cieList from '../../cie/redux';
+import recruitList from '../../recruiters/redux';
 import utils from './utils';
 
 export default combineReducers({
   newJob,
   cieList,
+  recruitList,
   utils,
 });
